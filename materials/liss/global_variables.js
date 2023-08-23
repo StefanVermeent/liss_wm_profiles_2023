2c249fc016d125e9bc480e614b749fd0b832b692
Stefan <p.c.s.vermeent@gmail.com>
2023-08-23 10:49:04 +0200

    change full screen mode language

--- materials/liss/global_variables.js
+++ materials/liss/global_variables.js
@@ -2,7 +2,7 @@
 var fullscreenmode = {
   type: jsPsychFullscreen,
   fullscreen_mode: true,
-  message: "<p>Het experiment zal verder gaan in <i>volledig scherm modus</i>.<br>We verzoeken je om in <i>volledig scherm modus</i> te blijven tijdens de gehele duur van het experiment.</p>",
+  message: "<p>Het experiment zal verder gaan in <i>volledig scherm modus</i>.<br>We verzoeken u om in <i>volledig scherm modus</i> te blijven tijdens de gehele duur van het experiment.</p>",
   button_label: "Ga verder"
 };
 
