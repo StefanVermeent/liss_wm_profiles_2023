cdbaf5db7248889bd2dc348452600cb9d5a4757e
Stefan <p.c.s.vermeent@gmail.com>
2024-06-25 13:53:02 +0200

    supplemental materials for Stage 2 manuscript

--- supplement/scripts/stage_supp.R
+++ supplement/scripts/stage_supp.R
@@ -1,10 +1,11 @@
 library(flextable)
 library(tidyverse)
+library(patchwork)
 
 load("data/pilot_full_data.RData")
 source("scripts/0_custom_functions/binom_test.R")
 source("scripts/0_custom_functions/corr_plot.R")
-
+clean_data <- read_csv("data/liss_data/clean_data.csv")
 
 # set up flextable for tables
 set_flextable_defaults(
@@ -22,16 +23,16 @@ set_flextable_defaults(
 
 pilot_demo <-
   list(
-    m_age = pilot_data_full$dems_age |> mean(na.rm = T) |> round(1),
-    sd_age = pilot_data_full$dems_age |> sd(na.rm = T) |> round(1),
-    max_age = pilot_data_full$dems_age |> max(na.rm = T) |> round(1),
-    min_age = pilot_data_full$dems_age |> min(na.rm = T) |> round(1)
+    m_age = pilot_full_data$dems_age |> mean(na.rm = T) |> round(1),
+    sd_age = pilot_full_data$dems_age |> sd(na.rm = T) |> round(1),
+    max_age = pilot_full_data$dems_age |> max(na.rm = T) |> round(1),
+    min_age = pilot_full_data$dems_age |> min(na.rm = T) |> round(1)
   )
 
 pilot_exclusions <-
   list(
-    acc_ospan_sec_n = pilot_data_full |> filter(ospan_sec_acc*100 < gbinom(45, 0.50)) |> nrow(),
-    acc_rspan_sec_n = pilot_data_full |> filter(rspan_sec_acc*100 < gbinom(45, 0.50)) |> nrow()
+    acc_ospan_sec_n = pilot_full_data |> filter(ospan_sec_acc*100 < gbinom(45, 0.50)) |> nrow(),
+    acc_rspan_sec_n = pilot_full_data |> filter(rspan_sec_acc*100 < gbinom(45, 0.50)) |> nrow()
   )
 
 pilot_clean <- pilot_full_data |>
@@ -90,3 +91,221 @@ pilot_table1 <- pilot_clean |>
 
 save(pilot_demo, pilot_exclusions, pilot_table1, file = "supplement/staged_results.RData")
 
+
+
+# 2. IV histograms --------------------------------------------------------
+
+## 2.1 Perceived deprivation (mean) ----
+
+hist_data <- clean_data |>
+  select(inr_m, live_off_income_m, finan_trouble_m, curr_situation_m, p_scar_m,
+         inr_cv, live_off_income_cv, finan_trouble_cv, curr_situation_cv, p_scar_cv,
+         neigh_thr_m, neigh_thr02_m, vict_sum, threat_comp)
+
+inr_m_hist <- hist_data |>
+  select(inr_m) |>
+  ggplot(aes(inr_m)) +
+  geom_histogram() +
+  theme_classic() +
+  scale_x_continuous(breaks = seq(0, 6, 1)) +
+  labs(
+    x = "Income-to-needs ratio (mean)",
+    y = "Frequency"
+  )
+
+live_off_income_m_hist <- hist_data |>
+  select(live_off_income_m) |>
+  ggplot(aes(live_off_income_m)) +
+  geom_histogram() +
+  theme_classic() +
+  scale_x_continuous(breaks = seq(0, 11, 1)) +
+  labs(
+    x = "How hard or how easy it is for you to\nlive off the income of your household?",
+    y = "Frequency"
+  )
+
+
+finan_trouble_m_hist <- hist_data |>
+  select(finan_trouble_m) |>
+  ggplot(aes(finan_trouble_m)) +
+  geom_histogram() +
+  theme_classic() +
+ # scale_x_continuous(breaks = seq(0, 6, 1)) +
+  xlim(c(0,6)) +
+  labs(
+    x = "With which of the following issues are you\nor are you not confronted at present?",
+    y = "Frequency"
+  )
+
+curr_situation_m_hist <- hist_data |>
+  select(curr_situation_m) |>
+  ggplot(aes(curr_situation_m)) +
+  geom_histogram() +
+  theme_classic() +
+  scale_x_continuous(breaks = seq(0, 11, 1)) +
+  labs(
+    x = "How would you describe the financial situation\nof your household at this moment?",
+    y = "Frequency"
+  )
+
+
+p_scar_m_hist <- hist_data |>
+  select(p_scar_m) |>
+  ggplot(aes(p_scar_m)) +
+  geom_histogram() +
+  theme_classic() +
+  scale_x_continuous(breaks = seq(0, 11, 1)) +
+  labs(
+    x = "Material deprivation compositite",
+    y = "Frequency"
+  )
+
+
+p_scar_m_combn_hist <- (live_off_income_m_hist + finan_trouble_m_hist) /
+(curr_situation_m_hist + p_scar_m_hist)
+
+## 2.2 INR (mean) ----
+
+inr_m_hist <- hist_data |>
+  select(inr_m) |>
+  ggplot(aes(inr_m)) +
+  geom_histogram() +
+  theme_classic() +
+  scale_x_continuous(breaks = seq(0, 11, 1)) +
+  labs(
+    x = "Income-to-needs ratio",
+    y = "Frequency"
+  )
+
+
+## 2.3 Threat ----
+
+nvs_hist <- hist_data |>
+  select(neigh_thr02_m) |>
+  ggplot(aes(neigh_thr02_m)) +
+  geom_histogram() +
+  theme_classic() +
+  scale_x_continuous(breaks = seq(0, 11, 1)) +
+  labs(
+    x = "Neighborhood Violence Scale",
+    y = "Frequency"
+  )
+
+
+neigh_hist <- hist_data |>
+  select(neigh_thr_m) |>
+  ggplot(aes(neigh_thr_m)) +
+  geom_histogram() +
+  theme_classic() +
+  scale_x_continuous(breaks = seq(0, 11, 1)) +
+  labs(
+    x = "Neighborhood Safety",
+    y = "Frequency"
+  )
+
+vict_hist <- hist_data |>
+  select(vict_sum) |>
+  ggplot(aes(vict_sum)) +
+  geom_histogram() +
+  theme_classic() +
+  scale_x_continuous(breaks = seq(0, 11, 1)) +
+  labs(
+    x = "Crime victimization",
+    y = "Frequency"
+  )
+
+
+threat_comp_hist <- hist_data |>
+  select(threat_comp) |>
+  ggplot(aes(threat_comp)) +
+  geom_histogram() +
+  theme_classic() +
+  scale_x_continuous(breaks = seq(0, 11, 1)) +
+  labs(
+    x = "Threat composite",
+    y = "Frequency"
+  )
+
+threat_combn_hist <-
+  (nvs_hist + neigh_hist) /
+  (vict_hist + threat_comp_hist)
+
+
+## 2.4 Perceived deprivation (CV) ----
+
+inr_cv_hist <- hist_data |>
+  select(inr_cv) |>
+  ggplot(aes(inr_cv)) +
+  geom_histogram() +
+  theme_classic() +
+  scale_x_continuous(breaks = seq(0, 2, 0.2)) +
+  labs(
+    x = "Income-to-needs ratio (CV)",
+    y = "Frequency"
+  )
+
+live_off_income_cv_hist <- hist_data |>
+  select(live_off_income_cv) |>
+  ggplot(aes(live_off_income_cv)) +
+  geom_histogram() +
+  theme_classic() +
+  scale_x_continuous(breaks = seq(0, 2, 0.2)) +
+  labs(
+    x = "How hard or how easy it is for you to\nlive off the income of your household?",
+    y = "Frequency"
+  )
+
+
+finan_trouble_cv_hist <- hist_data |>
+  select(finan_trouble_cv) |>
+  ggplot(aes(finan_trouble_cv)) +
+  geom_histogram() +
+  theme_classic() +
+  scale_x_continuous(breaks = seq(0, 2, 0.2)) +
+  labs(
+    x = "With which of the following issues are you\nor are you not confronted at present?",
+    y = "Frequency"
+  )
+
+curr_situation_cv_hist <- hist_data |>
+  select(curr_situation_cv) |>
+  ggplot(aes(curr_situation_cv)) +
+  geom_histogram() +
+  theme_classic() +
+  scale_x_continuous(breaks = seq(0, 2, 0.2)) +
+  labs(
+    x = "How would you describe the financial situation\nof your household at this moment?",
+    y = "Frequency"
+  )
+
+
+p_scar_cv_hist <- hist_data |>
+  select(p_scar_cv) |>
+  ggplot(aes(p_scar_cv)) +
+  geom_histogram() +
+  theme_classic() +
+  labs(
+    x = "Material deprivation compositite",
+    y = "Frequency"
+  )
+
+
+p_scar_cv_combn_hist <- (live_off_income_cv_hist + finan_trouble_cv_hist) /
+  (curr_situation_cv_hist + p_scar_cv_hist)
+
+## 2.5 INR (CV) ----
+
+inr_cv_hist <- hist_data |>
+  select(inr_cv) |>
+  ggplot(aes(inr_cv)) +
+  geom_histogram() +
+  theme_classic() +
+  scale_x_continuous(breaks = seq(0, 2, 0.2)) +
+  labs(
+    x = "Income-to-needs ratio",
+    y = "Frequency"
+  )
+
+save(pilot_demo, pilot_exclusions, pilot_table1,
+     p_scar_m_combn_hist, inr_m_hist, threat_combn_hist, p_scar_cv_combn_hist, inr_cv_hist,
+     file = "supplement/staged_results.RData")
